0ac0dd4943bd14830558141a1c891cf730e869c8
utshobbose <utshob.bose@g.bracu.ac.bd>
2025-07-11 15:44:45 +0600

    AuthProvider Build
    On progress, not completed!

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/authcontext.jsx';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+    </BrowserRouter>
+    
   </StrictMode>,
 )
